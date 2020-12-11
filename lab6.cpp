48b05dbd3a9e8f72bd20b7d126d73018575268e1
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2020-12-11 07:38:30 +0000

    Lab-6-Zadanie-1-v2

--- lab6.cpp
+++ lab6.cpp
@@ -1,29 +1,6 @@
 #include <iostream>
 
-#include <algorithm>
-#include <random>
-#include <type_traits>
-#include <vector>
-
-template < typename T >
-std::vector< T > make_random_vector(size_t size, T min_value, T max_value)
-{
-    static_assert(std::is_arithmetic_v< T >, "T must be an arithmetic type");
-
-    std::mt19937     prng{std::random_device{}()};
-    std::vector< T > ret_v;
-    ret_v.reserve(size);
-    auto bi = std::back_inserter(ret_v);
-
-    using dist_t = std::conditional_t< std::is_integral_v< T >,
-                                       std::uniform_int_distribution< T >,
-                                       std::uniform_real_distribution< T > >;
-
-    dist_t dist{min_value, max_value};
-    std::generate_n(bi, size, [&]() { return dist(prng); });
-
-    return ret_v;
-}
+#include "make_random_vector.hpp"
 
 void print_vector(std::vector< int > wektor)
 {
@@ -41,6 +18,6 @@ int main()
 }
 
 /* Po uruchomieniu otrzymano:
-  10 10 6 9 1 3 10 10 2 6 
+  10 10 6 9 1 3 10 10 2 6
   1 2 3 6 6 9 10 10 10 10
 */

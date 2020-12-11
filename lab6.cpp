9e6d9146786e166921f888ed87aaf128c9f1be01
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2020-12-11 08:11:14 +0000

    Lab-6-Zadanie-5

--- lab6.cpp
+++ lab6.cpp
@@ -14,21 +14,40 @@ bool czyWieksze(int i, int j)
     return (i > j);
 }
 
+bool czyWiekszeOdSiedem(int i)
+{
+    return (i > 7);
+}
+
+// Zmienna globalna
+int a;
+
+bool czyWiekszeOda(int i)
+{
+    return (i > a);
+}
+
 int main()
 {
     std::vector< int > wektor = make_random_vector< int >(10, 0, 10);
     std::cout << "Wygenerowany wektor:" << std::endl;
     print_vector(wektor);
-    std::sort(wektor.begin(), wektor.end(), czyWieksze);
-    std::cout << "Wektor posortowany malejaco" << std::endl;
+    std::sort(wektor.begin(), wektor.end());
+    std::cout << "Posortowany wektor:" << std::endl;
     print_vector(wektor);
-    // std::cout << "Liczba wystapien elementu 7:" << std::endl;
-    // std::cout << std::count(wektor.begin(), wektor.end(), 7) << std::endl;
+    std::cout << "Podaj liczbe:" << std::endl;
+    std::cin >> a;
+    std::cout << "Liczba wystapien elementow wiekszych od podanej liczby:" << std::endl;
+    std::cout << std::count_if(wektor.begin(), wektor.end(), czyWiekszeOda) << std::endl;
 }
 
 /* Po uruchomieniu otrzymano:
   Wygenerowany wektor:
-  6 8 0 6 8 8 9 0 5 4 
-  Wektor posortowany malejaco
-  9 8 8 8 6 6 5 4 0 0 
+  10 7 2 9 6 4 4 1 0 6 
+  Posortowany wektor:
+  0 1 2 4 4 6 6 7 9 10 
+  Podaj liczbe:
+  2
+  Liczba wystapien elementow wiekszych od podanej liczby:
+  7
 */

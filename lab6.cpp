244620395997821540361ef60494e04669fad4b6
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2020-12-11 08:22:21 +0000

    Lab-6-Zadanie-6

--- lab6.cpp
+++ lab6.cpp
@@ -19,13 +19,13 @@ bool czyWiekszeOdSiedem(int i)
     return (i > 7);
 }
 
-// Zmienna globalna
-int a;
-
-bool czyWiekszeOda(int i)
+class czyWiekszeOda
 {
-    return (i > a);
-}
+public:
+    czyWiekszeOda(int a_) : a{a_} {}
+    int  a;
+    bool operator()(int i) { return (i > a); }
+};
 
 int main()
 {
@@ -35,19 +35,21 @@ int main()
     std::sort(wektor.begin(), wektor.end());
     std::cout << "Posortowany wektor:" << std::endl;
     print_vector(wektor);
+    int a;
     std::cout << "Podaj liczbe:" << std::endl;
     std::cin >> a;
+    czyWiekszeOda czyWiekszeOdPodanea{a};
     std::cout << "Liczba wystapien elementow wiekszych od podanej liczby:" << std::endl;
-    std::cout << std::count_if(wektor.begin(), wektor.end(), czyWiekszeOda) << std::endl;
+    std::cout << std::count_if(wektor.begin(), wektor.end(), czyWiekszeOdPodanea) << std::endl;
 }
 
 /* Po uruchomieniu otrzymano:
   Wygenerowany wektor:
-  10 7 2 9 6 4 4 1 0 6 
+  8 2 5 2 10 1 10 0 1 6 
   Posortowany wektor:
-  0 1 2 4 4 6 6 7 9 10 
+  0 1 1 2 2 5 6 8 10 10 
   Podaj liczbe:
   2
   Liczba wystapien elementow wiekszych od podanej liczby:
-  7
+  5
 */

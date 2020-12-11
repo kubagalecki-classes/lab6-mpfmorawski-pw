608909ca42e37df65c816ce8a4c5f1f2852b4a36
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2020-12-11 08:36:18 +0000

    Lab-6-Zadanie-7

--- lab6.cpp
+++ lab6.cpp
@@ -38,18 +38,18 @@ int main()
     int a;
     std::cout << "Podaj liczbe:" << std::endl;
     std::cin >> a;
-    czyWiekszeOda czyWiekszeOdPodanea{a};
     std::cout << "Liczba wystapien elementow wiekszych od podanej liczby:" << std::endl;
-    std::cout << std::count_if(wektor.begin(), wektor.end(), czyWiekszeOdPodanea) << std::endl;
+    std::cout << std::count_if(wektor.begin(), wektor.end(), [&](int i) { return (i > a); })
+              << std::endl;
 }
 
 /* Po uruchomieniu otrzymano:
   Wygenerowany wektor:
-  8 2 5 2 10 1 10 0 1 6 
+  3 8 7 6 6 9 8 10 9 5 
   Posortowany wektor:
-  0 1 1 2 2 5 6 8 10 10 
+  3 5 6 6 7 8 8 9 9 10 
   Podaj liczbe:
-  2
+  3
   Liczba wystapien elementow wiekszych od podanej liczby:
-  5
+  9
 */

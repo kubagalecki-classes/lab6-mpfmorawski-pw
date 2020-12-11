9893cecdd148756ec3826f819e276b0a50bc192f
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2020-12-11 07:53:02 +0000

    Lab-6-Zadanie-3

--- lab6.cpp
+++ lab6.cpp
@@ -9,19 +9,28 @@ void print_vector(std::vector< int > wektor)
     std::cout << std::endl;
 }
 
+bool czyWieksze(int i, int j)
+{
+    return (i > j);
+}
+
 int main()
 {
     std::vector< int > wektor = make_random_vector< int >(10, 0, 10);
     std::cout << "Wygenerowany wektor:" << std::endl;
     print_vector(wektor);
-    std::sort(wektor.begin(), wektor.end());
-    std::cout << "Posortowany wektor:" << std::endl;
+    std::sort(wektor.begin(), wektor.end(), czyWieksze);
+    std::cout << "Wektor posortowany malejaco" << std::endl;
     print_vector(wektor);
-    std::cout << "Liczba wystapien elementu 7:" << std::endl;
-    std::cout << std::count(wektor.begin(), wektor.end(), 7) << std::endl;
+    // std::cout << "Liczba wystapien elementu 7:" << std::endl;
+    // std::cout << std::count(wektor.begin(), wektor.end(), 7) << std::endl;
 }
 
 /* Po uruchomieniu otrzymano:
-  10 10 6 9 1 3 10 10 2 6
-  1 2 3 6 6 9 10 10 10 10
+  Wygenerowany wektor:
+  3 1 2 0 6 3 0 7 10 4
+  Posortowany wektor:
+  0 0 1 2 3 3 4 6 7 10
+  Liczba wystapien elementu 7:
+  1
 */

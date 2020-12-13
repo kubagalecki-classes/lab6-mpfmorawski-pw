d3165d7774e85354ebd78cf69edea98297ddbc61
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2020-12-13 15:54:33 +0000

    Lab-6-Cwiczenie-IV

--- lab6.cpp
+++ lab6.cpp
@@ -173,15 +173,25 @@ void makeInputReversed()
     std::cout << s << std::endl;
 }
 
+/* Ćwiczenie IV - std::inner_product */
+
+void scalarProductExample()
+{
+    std::vector< float > first{1., 2., 3.};
+    std::vector< float > second{1.1, 2.2, 3.3};
+    float                ip = std::inner_product(first.begin(), first.end(), second.begin(), 0.);
+    std::cout << "Iloczny sklarny:" << std::endl;
+    /* 1. * 1.1 + 2. * 2.2 + 3. * 3.3. = 1.1 + 4.4. + 9.9 = 15.4 */
+    std::cout << ip << std::endl;
+}
+
 int main()
 {
-    makeInputReversed();
+    scalarProductExample();
     return 0;
 }
 
 /* Po uruchomieniu otrzymano:
-  Podaj ciag znakow
-  abcd
-  Ciag znakow od tylu
-  dcba
+  Iloczny sklarny:
+  15.4
 */

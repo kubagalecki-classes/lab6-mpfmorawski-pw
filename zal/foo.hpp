f049cae614ed86be46fb0c6770fd5bec24c0ec77
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2020-12-13 20:47:07 +0000

    Lab-6-Zal-1

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -10,7 +10,15 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
 
-    // Twoja implementacja tutaj
+    std::transform(
+       people.begin(), people.end(), ret_v.begin(), [](Human i) 
+       { 
+         if(i->isMonster())
+          return 'n';
+         else
+          return 'y';
+      });
+    std::reverse(ret_v.begin(), ret_v.end())
 
     return ret_v;
 }

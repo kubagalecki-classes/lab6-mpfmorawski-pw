b7598da56d8d453130ecf5954e54382f11f2ffbc
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2020-12-13 21:20:15 +0000

    Lab-6-Zal

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -11,7 +11,7 @@ std::vector< char > foo(std::list< Human >& people)
     std::vector< char > ret_v(people.size());
 
     std::transform(
-       people.begin(), people.end(), ret_v.begin(), [](Human& i) 
+       people.begin(), people.end(), ret_v.rbegin(), [](Human& i) 
        { 
          if(i.isMonster())
           return 'n';
@@ -23,7 +23,5 @@ std::vector< char > foo(std::list< Human >& people)
       i.birthday();
     });
 
-
-    std::reverse(ret_v.begin(), ret_v.end());
     return ret_v;
 }
